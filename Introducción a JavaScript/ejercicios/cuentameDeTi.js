0ea45f8d5e865b74221d1f669ae2a3be5c1b1ee6
Jonathan Vazquez <yair.vazquez001@gmail.com>
2021-12-15 12:23:36 -0600

    Todos los ejercicios pasados

---
+++ Introducción a JavaScript/ejercicios/cuentameDeTi.js
@@ -0,0 +1,10 @@
+/* Nombre, numero favorito, responder: Tienes mascota */
+
+let miNombre = 'Jonathan Yair';
+let numeroFav = 7;
+let tenerMascota = true;
+
+console.log('Mi nombre es ' + miNombre 
+            + ', mi numero favorito es ' + numeroFav 
+            + ', si me preguntas ¿Tienes mascota? La respuesta es ' + tenerMascota
+            );
---
+++ bootcamp-cohorte-8/javascript/01-variable.js
@@ -0,0 +1,12 @@
+/* 
+
+Variables: Nos sirven para almacenar información
+
+*/
+
+var nombre = 'Jonathan';
+let edad = 22;
+var mascota = false;
+
+console.log(nombre, edad, mascota);
+console.log(nombre + edad + mascota);
